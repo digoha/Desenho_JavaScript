00378eddc98aac0259c8b2790989eac131e72b2b
Rodrigo H. Albernaz <rodrigoha2001@hotmail.com>
2023-04-14 10:06:10 -0300

    add arquivos js

---
+++ src/js/cubo.js
@@ -0,0 +1,39 @@
+function draw() {
+    const canvas = document.getElementById("area");
+    const ctx = canvas.getContext("2d");
+    //Codigo do desenho
+    ctx.lineWidth = 1;
+    ctx.fillStyle = "#00931b";
+    /*ctx.beginPath();
+    ctx.moveTo(175, 275);
+    ctx.lineTo(175, 175);
+    ctx.lineTo(275, 175);
+    ctx.lineTo(275, 275);
+    ctx.lineTo(175, 275);*/
+    ctx.rect(175, 175, 100, 100)
+    ctx.stroke();
+    ctx.fill();
+
+    ctx.fillStyle = "yellow";
+    ctx.beginPath();
+    ctx.moveTo(175, 175);
+    ctx.lineTo(250-10, 125);
+    ctx.lineTo(350-10, 125);
+    ctx.lineTo(275, 175);
+    ctx.lineTo(175, 175);
+    ctx.stroke();
+    ctx.fill();
+
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.moveTo(275, 175);
+    ctx.lineTo(350-10, 125);
+    ctx.lineTo(350-10, 225);
+    ctx.lineTo(275, 275);
+    ctx.lineTo(275, 175);
+    ctx.stroke();
+    ctx.fill();
+
+
+}
+window.addEventListener("load", draw);
---
+++ src/js/telaDeJogo.js
@@ -0,0 +1,7 @@
+function draw() {
+    const canvas = document.getElementById("area");
+    const ctx = canvas.getContext("2d");
+    //Codigo do desenho
+    
+}
+window.addEventListener("load", draw);
